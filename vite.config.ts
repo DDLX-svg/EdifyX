7bc1333e4b3e0b1fe56550f58b93d6d1e62bae6b
DDLX-svg <jamesamaiden@gmail.com>
2025-09-03 16:42:18 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: '/SuniMed/', // nếu deploy GH Pages, Netlify thì dùng '/'
+    base: '/SuniMed/',
     plugins: [react()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'), // import từ src dễ hơn
-      },
+        '@': path.resolve(__dirname, './src')
+      }
     },
     define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     }
   };
