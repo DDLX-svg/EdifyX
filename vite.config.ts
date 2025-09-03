19a8a8e895ce62f10bceace9c3f0266beb325821
DDLX-svg <jamesamaiden@gmail.com>
2025-09-03 16:26:52 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: '/SuniMed/', // base path cho GitHub Pages
+    base: '/', // cho Netlify
     plugins: [react()],
     resolve: {
       alias: {
